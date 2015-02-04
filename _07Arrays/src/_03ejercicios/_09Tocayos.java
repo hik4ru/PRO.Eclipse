e83ad25a0f2f613fee7f45505dc4a0d11b0bdc0c
Hikaru <Hikaru@Hikaru-PC>
2015-02-04 17:36:05 +0100

    04/02/2015 17:36

---
+++ _07Arrays/src/_03ejercicios/_09Tocayos.java
@@ -0,0 +1,18 @@
+package _03ejercicios;
+
+public class _09Tocayos {
+
+	public static void main(String[] args) {
+		
+		String grupo1[] = {"miguel","josé","ana","maría"};
+		String grupo2[] = {"ana", "josé", "luján", "juan","josé", "pepa", "ángela", "sofía", "andrés", "bartolo"};
+		
+	}
+	
+	public static void tocayos(String v1[], String v2[]){
+		for (int i=0; i<v1.length; i++){
+			
+		}
+	}
+	
+}
---
+++ _07Arrays/src/_03ejercicios/_11ValorPar.java
@@ -0,0 +1,99 @@
+package _03ejercicios;
+
+public class _11ValorPar {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static boolean haypares1(int v[]) {
+		int i = 0;
+		while (i < v.length && v[i] % 2 != 0) {
+			i++;
+		}
+		if (v[i] % 2 == 0)
+			return true;
+		else
+			return false;
+		
+		//NO FUNCIONA
+	}
+
+	public static boolean haypares2(int v[]) {
+		int i = 0;
+		while (i < v.length && v[i] % 2 != 0) {
+			i++;
+		}
+		if (i < v.length)
+			return true;
+		else
+			return false;
+		
+		//SI FUNCIONA
+	}
+
+	public static boolean haypares3(int v[]) {
+		int i = 0;
+		while (v[i] % 2 != 0 && i < v.length) {
+			i++;
+		}
+		if (i < v.length)
+			return true;
+		else
+			return false;
+		
+		//SI FUNCIONA
+	}
+
+	public static boolean haypares4(int v[]) {
+		int i = 0;
+		boolean encontrado = false;
+		while (i <= v.length && !encontrado) {
+			if (v[i] % 2 == 0)
+				encontrado = true;
+			else
+				i++;
+		}
+		return encontrado;
+		
+		//NO FUNCIONA
+	}
+
+	public static boolean haypares5(int v[]) {
+		int i = 0;
+		boolean encontrado = false;
+		while (i < v.length && !encontrado) {
+			if (v[i] % 2 == 0)
+				encontrado = true;
+			i++;
+		}
+		return encontrado;
+		
+		//SI FUNCIONA
+	}
+
+	public static boolean haypares6(int v[]) {
+		int i = 0;
+		while (i < v.length) {
+			if (v[i] % 2 == 0)
+				return true;
+			else
+				return false;
+		}
+		
+		//NO FUCNIONA
+	}
+
+	public static boolean haypares7(int v[]) {
+		int i = 0;
+		while (i < v.length) {
+			if (v[i] % 2 == 0)
+				return true;
+			i++;
+		}
+		return false;
+		
+		//SI FUCNIONA
+	}
+
+}
